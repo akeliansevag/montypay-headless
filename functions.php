fe3e8ac6266da9d9ffe46eaabd18324a75ac28f1
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-04-17 12:05:02 +0300

    .

--- functions.php
+++ functions.php
@@ -1,12 +1,17 @@
 <?php
-	function get_social_media_icons(){
-		return get_field("social_media_icons","options");
-	}
-	
-	add_action('rest_api_init', function(){
-		register_rest_route( 'portfolio', '/social-media-icons', array(
-			'methods' => 'GET',
-			'callback' => 'get_social_media_icons',
-		));
-	});
-?>
+// function get_social_media_icons(){
+// 	return get_field("social_media_icons","options");
+// }
+
+// add_action('rest_api_init', function(){
+// 	register_rest_route( 'portfolio', '/social-media-icons', array(
+// 		'methods' => 'GET',
+// 		'callback' => 'get_social_media_icons',
+// 	));
+// });
+function my_theme_setup()
+{
+	add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'my_theme_setup');
