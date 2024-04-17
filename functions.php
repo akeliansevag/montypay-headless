b0db0dc02d1ffa365ed17397ef3dbdfa5c1254b5
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-04-17 12:11:27 +0300

    .

--- functions.php
+++ functions.php
@@ -12,6 +12,15 @@
 function my_theme_setup()
 {
 	add_theme_support('post-thumbnails');
+	add_image_size('large', 700, '', true); // Large Thumbnail
+	add_image_size('medium', 250, '', true); // Medium Thumbnail
+	add_image_size('medium-square', 250, 250, true); // Medium Square
+	add_image_size('small-square', 120, 120, true); // Small Square
+	add_image_size('avatar', 50, 50, true); // Small Square
+	add_image_size('post-thumbnail', 500, 420, true); // Post Thumbnail
+	add_image_size('small', 120, '', true); // Small Thumbnail
+	add_image_size('banner-image', 1920, 1080, true);
+	add_image_size('portfolio-thumbnail', 300, 200, true); // Custom Thumbnail Size call using
 }
 
 add_action('after_setup_theme', 'my_theme_setup');
